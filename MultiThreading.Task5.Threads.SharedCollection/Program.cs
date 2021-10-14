a1915073bb133a78aa9abcfb6c7a0bca22c0da07
Vilmantas Sukauskas <Vilmantas_Sukauskas@epam.com>
2021-10-14 09:41:38 +0300

    Task 5

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,20 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private static AutoResetEvent _readLock = new AutoResetEvent(false);
+        private static AutoResetEvent _writeLock = new AutoResetEvent(true);
+        
+        private static List<int> _sharedCollection = new List<int>();
+        private static int _maxCollectionSize = 10;
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -18,6 +27,25 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine();
 
             // feel free to add your code
+            new TaskFactory().StartNew(() =>
+            {
+                for (int i = 1; i <= _maxCollectionSize; i++)
+                {
+                    _writeLock.WaitOne();
+                    _sharedCollection.Add(i);
+                    _readLock.Set();
+                }
+            });
+
+            new TaskFactory().StartNew(() =>
+            {
+                for (int i = 1; i <= _maxCollectionSize; i++)
+                {
+                    _readLock.WaitOne();
+                    Console.WriteLine(string.Join(", ", _sharedCollection));
+                    _writeLock.Set();
+                }
+            });
 
             Console.ReadLine();
         }
